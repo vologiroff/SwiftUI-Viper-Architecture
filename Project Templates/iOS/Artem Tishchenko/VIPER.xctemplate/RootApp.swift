f7ac6c602b8414238ca9ef200359496e72aba460
Artem Tishenko <maukur@gmail.com>
2023-10-16 01:11:41 +0400

    Initial commit

---
+++ Project Templates/iOS/Artem Tishchenko/VIPER.xctemplate/RootApp.swift
@@ -0,0 +1,47 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//  ___COPYRIGHT___
+//
+
+import SwiftUI
+
+@main
+class RootApp: App {
+    
+    @ObservedObject var appViewBuilder: ApplicationViewBuilder
+    @ObservedObject var navigationService: NavigationService
+    
+    let container: DependencyContainer = {
+        let factory = AssemblyFactory()
+        let container = DependencyContainer(assemblyFactory: factory)
+        
+        container.apply(NavigationAssembly.self)
+        
+        // Services
+        container.apply(NavigationAssembly.self)
+    
+        // Modules
+        container.apply(MainAssembly.self)
+
+        return container
+    }()
+
+    required init() {
+        navigationService = container.resolve(NavigationAssembly.self).build() as! NavigationService
+        
+        appViewBuilder = ApplicationViewBuilder(container: container)
+    }
+    
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView(navigationService: navigationService,
+                     appViewBuilder: appViewBuilder)
+        }
+    }
+    
+    
+}
